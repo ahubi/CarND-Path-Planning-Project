aef7bc809d745fdc196c269aa8e86a1ba5885bea
artur <babalasoft@gmail.com>
2018-01-19 20:30:38 +0100

    Move vehicle using spline function to generate jerk free path.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -34,33 +35,35 @@ string hasData(string s) {
   return "";
 }
 
-double distance(double x1, double y1, double x2, double y2)
-{
-  return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+double distance(double x1, double y1, double x2, double y2) {
+  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
-int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
-{
 
-  double closestLen = 100000; //large number
+int ClosestWaypoint(double x,
+                    double y,
+                    const vector<double> &maps_x,
+                    const vector<double> &maps_y) {
+
+  double closestLen = 100000; // large number
   int closestWaypoint = 0;
 
-  for(int i = 0; i < maps_x.size(); i++)
-  {
+  for (int i = 0; i < maps_x.size(); i++) {
     double map_x = maps_x[i];
     double map_y = maps_y[i];
-    double dist = distance(x,y,map_x,map_y);
-    if(dist < closestLen)
-    {
+    double dist = distance(x, y, map_x, map_y);
+    if (dist < closestLen) {
       closestLen = dist;
       closestWaypoint = i;
     }
   }
-
   return closestWaypoint;
-
 }
 
-int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+int NextWaypoint(double x,
+                 double y,
+                 double theta,
+                 const vector<double> &maps_x,
+                 const vector<double> &maps_y)
 {
 
   int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
@@ -86,7 +89,11 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+vector<double> getFrenet(double x,
+                         double y,
+                         double theta,
+                         const vector<double> &maps_x,
+                         const vector<double> &maps_y)
 {
   int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
 
@@ -133,7 +140,11 @@ vector<double> getFrenet(double x, double y, double theta, const vector<double> 
 }
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
+vector<double> getXY(double s,
+                     double d,
+                     const vector<double> &maps_s,
+                     const vector<double> &maps_x,
+                     const vector<double> &maps_y)
 {
   int prev_wp = -1;
 
@@ -176,7 +187,11 @@ int main() {
   double max_s = 6945.554;
 
   ifstream in_map_(map_file_.c_str(), ifstream::in);
-
+  if(!in_map_){
+    cerr << "Failed to read map data from " << map_file_
+        << " fatal error!" << endl;
+    exit(-1);
+  }
   string line;
   while (getline(in_map_, line)) {
   	istringstream iss(line);
@@ -196,9 +211,19 @@ int main() {
   	map_waypoints_dx.push_back(d_x);
   	map_waypoints_dy.push_back(d_y);
   }
+  int lane = 1; //0 most left lane, n - most right lane
+  double ref_vel = 49.5; //mph
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+  h.onMessage([&map_waypoints_x,
+               &map_waypoints_y,
+               &map_waypoints_s,
+               &map_waypoints_dx,
+               &map_waypoints_dy,
+               &lane,
+               &ref_vel]
+               (uWS::WebSocket<uWS::SERVER> ws,
+                char *data, size_t length,
+                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -234,18 +259,97 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
-          	json msgJson;
+            int prev_size = previous_path_x.size();
+            cout << "prev_size: " << prev_size << endl;
 
-          	vector<double> next_x_vals;
+            vector<double> ptsx;
+            vector<double> ptsy;
+            double ref_x   = car_x;
+            double ref_y   = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            if(prev_size < 2){
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+            }else{
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previous_path_y[prev_size-1];
+              double ref_x_prev = previous_path_x[prev_size-2];
+              double ref_y_prev = previous_path_y[prev_size-2];
+              ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+            }
+
+            vector<double> next_wp0 = getXY(car_s + 30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s + 60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s + 90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            for(int i = 0; i < ptsx.size(); i++){
+              //shift car reference angle to 0 degrees
+              double shift_x = ptsx[i] - ref_x;
+              double shift_y = ptsy[i] - ref_y;
+              ptsx[i] = (shift_x *cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+              ptsy[i] = (shift_x *sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+            }
+
+            tk::spline s;
+            s.set_points(ptsx, ptsy);
+
+            vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+            for (size_t i = 0; i < previous_path_x.size(); i++){
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+            double target_x = 30;
+            double target_y = s(target_x);
+            double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+            double x_add_on = 0;
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            for (size_t i = 1; i <= 50 - previous_path_x.size(); i++){
+              double N = (target_dist/(.02*ref_vel/2.24)); //2.24 - mph to mps
+              double x_point = x_add_on+(target_x)/N;
+              double y_point = s(x_point);
+
+              x_add_on = x_point;
+
+              double x_ref = x_point;
+              double y_ref = y_point;
+              x_point = (x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw));
+              y_point = (x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw));
+              x_point += ref_x;
+              y_point += ref_y;
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
+            }
+
+            json msgJson;
+            // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
-
+            //cout << msg << endl;
           	//this_thread::sleep_for(chrono::milliseconds(1000));
           	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
 
