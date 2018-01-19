eb052b51bff299baa84880bede297ca5d8137f2b
artur <babalasoft@gmail.com>
2018-01-19 21:10:19 +0100

    Remove unused code, add comments.

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@ using json = nlohmann::json;
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+//2.24 - mph to meter/s
+const double MPH_TO_MPS_CONSTANT = 2.24;
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -47,7 +49,7 @@ int ClosestWaypoint(double x,
   double closestLen = 100000; // large number
   int closestWaypoint = 0;
 
-  for (int i = 0; i < maps_x.size(); i++) {
+  for (size_t i = 0; i < maps_x.size(); i++) {
     double map_x = maps_x[i];
     double map_y = maps_y[i];
     double dist = distance(x, y, map_x, map_y);
@@ -260,7 +262,6 @@ int main() {
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
             int prev_size = previous_path_x.size();
-            cout << "prev_size: " << prev_size << endl;
 
             vector<double> ptsx;
             vector<double> ptsy;
@@ -290,7 +291,7 @@ int main() {
               ptsy.push_back(ref_y_prev);
               ptsy.push_back(ref_y);
             }
-
+            //add evenly 30m spaced points ahead of the starting reference
             vector<double> next_wp0 = getXY(car_s + 30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
             vector<double> next_wp1 = getXY(car_s + 60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
             vector<double> next_wp2 = getXY(car_s + 90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
@@ -303,12 +304,12 @@ int main() {
             ptsy.push_back(next_wp1[1]);
             ptsy.push_back(next_wp2[1]);
 
-            for(int i = 0; i < ptsx.size(); i++){
+            for(size_t i = 0; i < ptsx.size(); i++){
               //shift car reference angle to 0 degrees
               double shift_x = ptsx[i] - ref_x;
               double shift_y = ptsy[i] - ref_y;
-              ptsx[i] = (shift_x *cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
-              ptsy[i] = (shift_x *sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+              ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+              ptsy[i] = (shift_x * sin(0 - ref_yaw)+ shift_y * cos(0 - ref_yaw));
             }
 
             tk::spline s;
@@ -316,42 +317,46 @@ int main() {
 
             vector<double> next_x_vals;
           	vector<double> next_y_vals;
-
+            //use previous points
             for (size_t i = 0; i < previous_path_x.size(); i++){
               next_x_vals.push_back(previous_path_x[i]);
               next_y_vals.push_back(previous_path_y[i]);
             }
+
             double target_x = 30;
             double target_y = s(target_x);
-            double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+            double target_dist = sqrt(pow(target_x,2) + pow(target_y, 2));
             double x_add_on = 0;
-
+            /*
+              use previous not consumed points and fill up to 50 points with
+              new ones
+            */
             for (size_t i = 1; i <= 50 - previous_path_x.size(); i++){
-              double N = (target_dist/(.02*ref_vel/2.24)); //2.24 - mph to mps
-              double x_point = x_add_on+(target_x)/N;
+              double N = (target_dist / (.02 * ref_vel / MPH_TO_MPS_CONSTANT));
+              double x_point = x_add_on + (target_x) / N;
               double y_point = s(x_point);
 
               x_add_on = x_point;
 
               double x_ref = x_point;
               double y_ref = y_point;
+              //rotate back to normal after rotating it early
               x_point = (x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw));
               y_point = (x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw));
+
               x_point += ref_x;
               y_point += ref_y;
+
               next_x_vals.push_back(x_point);
               next_y_vals.push_back(y_point);
             }
 
             json msgJson;
-            // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-          	msgJson["next_x"] = next_x_vals;
+            msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
-            //cout << msg << endl;
-          	//this_thread::sleep_for(chrono::milliseconds(1000));
-          	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
 
         }
       } else {
